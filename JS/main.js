0646037d5b2e88ddf63ea38d5c91f5d7ea0c183d
Daniil <68585455+mrwhiteered@users.noreply.github.com>
2023-03-09 17:53:31 +0300

    r
    r

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
 document.addEventListener('copy', function(event) {
     event.preventDefault();
   });
+
+  
+  window.onload = function() {
+    // Delay for 5 seconds
+    setTimeout(function() {
+      // Remove the preload animation
+      document.getElementById("preload-animation").style.display = "none";
+    }, 500);
+  };
